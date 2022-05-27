42f0e3314ca4562aced464716d9849e6d30515ce
Mario Vidal Domínguez <Strikeroro@gmail.com>
2022-05-27 19:41:19 +0200

    section-7-directives

---
+++ section-7-directives/src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+
+import { AppComponent } from "./app.component";
+import { BasicHighlightDirective } from "./basic-highlight/basic-highlight.directive";
+import { UnlessDirective } from "./unless.directive";
+import { BetterHighlightDirective } from './better-highlight/better-highlight.directive';
+
+@NgModule({
+  declarations: [AppComponent, BasicHighlightDirective, UnlessDirective, BetterHighlightDirective],
+  imports: [BrowserModule, FormsModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
